be3afb4c7c331409bb16d9b666d3c00dd172fa02
VinhTranDuc <56251559+VinhTranDuc@users.noreply.github.com>
2023-07-10 23:30:51 +0700

    update home data

--- src/app/widgets/notifications-list/notifications-list.component.ts
+++ src/app/widgets/notifications-list/notifications-list.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { GlobalSettings } from 'src/app/shared/global.settings';
+import { SharedPropertyService } from 'src/app/shared/shared-property.service';
+import { SharedService } from 'src/app/shared/shared.service';
+import { SimpleBaseComponent } from 'src/app/shared/simple.base.component';
 
 @Component({
-  selector: 'app-notifications-list',
-  templateUrl: './notifications-list.component.html',
-  styleUrls: ['./notifications-list.component.scss']
+	selector: 'app-notifications-list',
+	templateUrl: './notifications-list.component.html',
+	styleUrls: ['./notifications-list.component.scss']
 })
-export class NotificationsListComponent implements OnInit {
+export class NotificationsListComponent extends SimpleBaseComponent {
 
-  constructor() { }
+	public dataItems: any[] = [];
+	constructor(public sharedService: SharedPropertyService,
+		public service: SharedService
+	) {
+		super(sharedService);
+		this.getPosts();
+	}
 
-  ngOnInit(): void {
-  }
+	getPosts() {
+		let options = {
+			skip: 0,
+			top: 6,
+			sort: 'created desc',
+			select:'id,title,photo,created'
+		};
+		this.dataItems = [];
+		this.dataProcessing = true;
+		this.service.getPosts(options).pipe(take(1)).subscribe({
+			next: (res: any) => {
+				if (res && res.value && res.value.length > 0) {
+					this.dataItems = res.value;
+					for (let item of this.dataItems) {
+						if (item.photo) {
+							item.pictureUrl = `${GlobalSettings.Settings.Server}/${item.photo}`;
+						}
+						if (item.created) {
+							item._created = this.sharedService.convertDateStringToMoment(item.created, this.offset);
+							item.createdView = item._created.format('DD/MM/YYYY hh:mm A');
+						}
+
+					}
+				}
+				this.dataProcessing = false;
+			}
+		})
+	}
 
 }
--- src/app/widgets/posts-banner/posts-banner.component.ts
+++ src/app/widgets/posts-banner/posts-banner.component.ts
@@ -1,38 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { take } from 'rxjs';
+import { GlobalSettings } from 'src/app/shared/global.settings';
+import { SharedPropertyService } from 'src/app/shared/shared-property.service';
+import { SharedService } from 'src/app/shared/shared.service';
+import { SimpleBaseComponent } from 'src/app/shared/simple.base.component';
 
 @Component({
 	selector: 'app-posts-banner',
 	templateUrl: './posts-banner.component.html',
 	styleUrls: ['./posts-banner.component.scss']
 })
-export class PostsBannerComponent implements OnInit {
+export class PostsBannerComponent extends SimpleBaseComponent {
 	public logoRightUrl = './assets/images/logo-right.png';
-	public dataItems: any = [
-		{
-			thumb: './assets/images/banner.jpg',
-			title: 'Education',
-			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nostrum?',
-		},
-		{
-			thumb: './assets/images/banner.jpg',
-			title: 'Education',
-			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nostrum?',
-		},
-		{
-			thumb: './assets/images/banner.jpg',
-			title: 'Education',
-			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nostrum?',
-		},
-		{
-			thumb: './assets/images/banner.jpg',
-			title: 'Education',
-			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nostrum?',
-		}
-	]
+	public dataItems: any = []
 	public bgImage = './assets/images/banner.jpg';
-	constructor() { }
+	constructor(public sharedService: SharedPropertyService,
+		public service: SharedService
+	) {
+		super(sharedService);
+		this.getPosts();
+	}
 
-	ngOnInit(): void {
+	getPosts() {
+		let options = {
+			skip: 0,
+			top: 4,
+			sort: 'created desc',
+			select: 'id,title,photo,created'
+		};
+		this.dataItems = [];
+		this.dataProcessing = true;
+		this.service.getPosts(options).pipe(take(1)).subscribe({
+			next: (res: any) => {
+				if (res && res.value && res.value.length > 0) {
+					this.dataItems = res.value;
+					for (let item of this.dataItems) {
+						if (item.photo) {
+							item.pictureUrl = `${GlobalSettings.Settings.Server}/${item.photo}`;
+						}
+						if (item.created) {
+							item._created = this.sharedService.convertDateStringToMoment(item.created, this.offset);
+							item.createdView = item._created.format('DD/MM/YYYY hh:mm A');
+						}
+
+					}
+				}
+				this.dataProcessing = false;
+			}
+		})
 	}
 
 }
